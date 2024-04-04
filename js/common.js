6c5c6cb47b0f4baabcf65bc9c10b2f17c60d5fec
jay <jay@hiqsum.com>
2024-04-04 23:45:15 +0900

    A

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
 $(document).ready(() => {
   $("#header").load("../html/header.html");
+  Array.from(document.querySelectorAll("button")).forEach((item) => {
+    item.addEventListener("click", function (e) {
+      const ripple = document.createElement("div"),
+        rect = item.getBoundingClientRect();
+      (ripple.className = "animate"),
+        (ripple.style.left = `${e.x - rect.left}px`),
+        (ripple.style.top = `${e.y - rect.top}px`),
+        (ripple.style.background = `#919eab7a`),
+        ripple.style.setProperty("--ripple-scale", item.offsetWidth),
+        item.append(ripple),
+        setTimeout(function () {
+          ripple.parentNode.removeChild(ripple);
+        }, 500);
+    });
+  });
 });
