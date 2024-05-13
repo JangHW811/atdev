d3128df465895587b2b728f41e9dd316d9914d57
jay <jay@hiqsum.com>
2024-05-13 19:22:16 +0900

    파일 드래그앤 드랍 후 ui변경 기능 추가

--- js/registModal.js
+++ js/registModal.js
@@ -1,46 +1,49 @@
 $(document).ready(() => {
-  $("#registModal .searchList input[type='checkbox']").on(
-    "change",
-    function () {
-      const checked = $(this).prop("checked");
-      if ($(this).hasClass("checkAll")) {
-        $("#registModal .searchList input[type='checkbox']").prop(
-          "checked",
-          checked
-        );
-      } else {
-        const checkedLength = $(
-          "#registModal .searchList input[type='checkbox']:checked"
-        ).length;
-        const allLength = $(
-          "#registModal .searchList input[type='checkbox']:not(.checkAll)"
-        ).length;
-        if (checkedLength === allLength) {
-          $("#registModal .searchList input[type='checkbox'].checkAll").prop(
-            "checked",
-            true
-          );
-        } else {
-          $("#registModal .searchList input[type='checkbox'].checkAll").prop(
-            "checked",
-            false
-          );
-        }
-      }
-    }
-  );
+  $("#registModal .fileDrop")
+    .on("dragenter", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+    })
+    .on("dragover", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $(this)
+        .removeClass("grayOpacity12Background")
+        .addClass("primaryOpacity12Background");
+    })
+    .on("dragleave", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $(this)
+        .removeClass("primaryOpacity12Background")
+        .addClass("grayOpacity12Background");
+    })
+    .on("drop", function (e) {
+      e.preventDefault();
+
+      const files = e.originalEvent.dataTransfer.files;
+      $(this).find('input[type="file"]').prop("files", files);
+      $(this).find(".fileUploadTemplate").hide();
+      $(this).find(".fileName").show().append(files[0].name);
+      $(this)
+        .closest(".inputContainer")
+        .find(".fileSize")
+        .text((files[0].size / 1024 / 1024).toFixed(1));
+    });
+
+  $("#fileupload").on("change", function () {
+    const files = $(this).prop("files");
+    console.log("??A?A??", files);
+    $(this).closest(".fileDrop").find(".fileUploadTemplate").hide();
+    $(this).closest(".fileDrop").find(".fileName").show().append(files[0].name);
+    $(this)
+      .closest(".inputContainer")
+      .find(".fileSize")
+      .text((files[0].size / 1024 / 1024).toFixed(1));
+  });
 
   $("#registModal .submit").on("click", function () {
     const callback = modalObject["registModal"]?.callback;
-    const checkedList = $(
-      "#registModal .searchList input[type='checkbox']:checked"
-    );
-
-    const resultList = [];
-    checkedList.each((index, element) => {
-      const name = $(element).closest("tr").find(".name").text();
-      resultList.push(name);
-    });
 
     callback && callback(resultList);
     closeModal(this);
