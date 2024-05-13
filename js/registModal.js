26e996f4c35a9e4d9adb14201ada4e37d9a48143
jay <jay@hiqsum.com>
2024-05-13 22:58:17 +0900

    파일 삭제 기능 추가 및 파일 여러번 올리면 기존내용 남아있던 문제 수정

--- js/registModal.js
+++ js/registModal.js
@@ -21,24 +21,38 @@ $(document).ready(() => {
     .on("drop", function (e) {
       e.preventDefault();
 
+      $(this)
+        .removeClass("primaryOpacity12Background")
+        .addClass("grayOpacity12Background");
       const files = e.originalEvent.dataTransfer.files;
-      $(this).find('input[type="file"]').prop("files", files);
+      $(this).find("#fileupload").prop("files", files);
       $(this).find(".fileUploadTemplate").hide();
-      $(this).find(".fileName").show().append(files[0].name);
+      $(this).find(".fileName").show().text(files[0].name);
+      $("#registModal .removeFile").show();
       $(this)
         .closest(".inputContainer")
         .find(".fileSize")
         .text((files[0].size / 1024 / 1024).toFixed(1));
     });
 
-  $("#fileupload").on("change", function () {
+  $("#registModal #fileupload").on("change", function () {
     const files = $(this).prop("files");
     $(this).closest(".fileDrop").find(".fileUploadTemplate").hide();
-    $(this).closest(".fileDrop").find(".fileName").show().append(files[0].name);
+    $(this).closest(".fileDrop").find(".fileName").show().text(files[0].name);
+    $("#registModal .removeFile").show();
     $(this)
       .closest(".inputContainer")
       .find(".fileSize")
       .text((files[0].size / 1024 / 1024).toFixed(1));
+  });
+
+  $("#registModal .removeFile").on("click", function (event) {
+    event.stopPropagation();
+    $(this).closest(".inputContainer").find(".fileUploadTemplate").show();
+    $(this).closest(".inputContainer").find(".fileName").hide().text("");
+    $(this).closest(".inputContainer").find("#fileupload").prop("files", null);
+    $(this).hide();
+    $(this).closest(".inputContainer").find(".fileSize").text("0");
   });
 
   $("#registModal .submit").on("click", function () {
